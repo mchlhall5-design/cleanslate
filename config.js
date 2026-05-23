10c5c9304f3e824c0221f63b8bfdc679a341cc29
mchlhall5-design <mchlhall5@gmail.com>
2026-05-22 23:12:22 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-window.CLEANSLATE_CONFIG = {
-  firebase: {
+window.CLEANSATE_CONFIG = {
+  APP_VERSION: "CleanSlate GitHub Pages Button Fix V4",
+
+  GOOGLE_CLIENT_ID: "512137679024-o2b1stjgp7lhpc8pj53v016q5cqkbc1k.apps.googleusercontent.com",
+
+  FIREBASE: {
     apiKey: "AIzaSyDlY_ZKDAbTCAvLwJksuQ03vMUN6TNGgd4",
     authDomain: "cleanslate-c9be5.firebaseapp.com",
     databaseURL: "https://cleanslate-c9be5-default-rtdb.firebaseio.com",
@@ -9,10 +13,8 @@ window.CLEANSLATE_CONFIG = {
     appId: "1:552021876760:web:c946e8f0e30e0e9fc8236d"
   },
 
-  gmail: {
-    clientId: "",
-    apiKey: ""
-  },
-
-  APP_VERSION: "CleanSlate Firebase Live"
+  MAX_LIST_PAGE_SIZE: 500,
+  MESSAGE_FETCH_CONCURRENCY: 6,
+  RENDER_LIMIT: 400,
+  CLEANUP_BATCH_SIZE: 1000
 };
