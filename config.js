c7480ca376bfd268c8390d13fc5433f42f44dedc
mchlhall5-design <mchlhall5@gmail.com>
2026-05-23 16:16:35 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CLEANSATE_CONFIG = {
-  APP_VERSION: "CleanSlate Full Reset Build v2",
+  APP_VERSION: "CleanSlate Server-Side Complete V1",
   GOOGLE_CLIENT_ID: "512137679024-o2b1stjgp7lhpc8pj53v016q5cqkbc1k.apps.googleusercontent.com",
   FIREBASE: {
     apiKey: "AIzaSyDlY_ZKDAbTCAvLwJksuQ03vMUN6TNGgd4",
@@ -11,8 +11,5 @@ window.CLEANSATE_CONFIG = {
     appId: "1:552021876760:web:c946e8f0e30e0e9fc8236d"
   },
   WORKER_URL: "https://cleanslate-render-worker.onrender.com",
-  WORKER_SECRET: "",
-  MAX_LIST_PAGE_SIZE: 500,
-  MESSAGE_FETCH_CONCURRENCY: 6,
-  RENDER_LIMIT: 400
+  RENDER_LIMIT: 500
 };
